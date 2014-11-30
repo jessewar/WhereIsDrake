d03839f7936ff9a1f88509f67fd8081b52054a4a
Nathan Roberson <nathaniroberson@gmail.com>
2014-11-30 16:55:46 -0500

    Fixing template stucture to seed

--- app/app.js
+++ app/app.js
@@ -1,35 +1,29 @@
-var express = require('express');
+'use strict';
+
+var PORT_LISTENER = 3001;
+console.log('I am listening to this port: http://localhost:%s', PORT_LISTENER);
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var appConfig = require('./config/appConfig.json');
+
 var app = express();
-var http = require('http');
-var twit = require('twit');
-var stylus = require('stylus')
-var nib = require('nib')
 
-var app = express()
+// all environments
+app.set('port', process.env.PORT || PORT_LISTENER);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: path.join(__dirname, appConfig.directories.publicDir) }));
+app.use(express.methodOverride());
+app.use(express.cookieParser('my v3ry s3cr3t C00k1e k3y d0nt y0u th1nk?'));
+app.use(express.session({
+    secret: 'my l1ttl3 s3cret s3ss10n k3y isnt it?',
+    maxAge: 3600000
+}));
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
-
-//Set the views directory
-app.set('views', __dirname + '/views')
-//Set the view engine as jase
-app.set('view engine', 'jade')
-//Set stylus as the middleware option
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
-  )
-})
 
 var twitter = new twit({
   consumer_key: 'TwNjohtHnx2becRqgycPzX1hH',
@@ -38,8 +32,6 @@ var twitter = new twit({
   access_token_secret: '6yE3qCxPLC85D9DFJr0lopIoy2SjNfV1mgoqK6Vfa5XK7'
 });
 
-
-app.set('port', (process.env.PORT || 5000));
 
 //Start of twitter api request using the twit package
 app.get('/twitter', function(request, response) {
@@ -92,7 +84,24 @@ function songkick_request(response2){
     var req = http.request(options, callback).end()  
 }
 
+//routes
+require('./routes/index')(app);
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, appConfig.directories.publicDir)));
+
+app.use(function (req, res, next) {
+    console.log('req.body: ' + JSON.stringify(req.body));
+    next();
+});
+
+
+// development only
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+}
+
 // Start listening
-app.listen(app.get('port'), function() {
-console.log("Node app is running at localhost:" + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
