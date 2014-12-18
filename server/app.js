d9ddbf03b859d4fc874109f4176c67eba49c4ce9
Jesse Warren <jesse.warren@live.com>
2014-12-18 07:10:44 -0800

    changed process.env.NODE_ENV back to default or development

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@
 'use strict';
 
 // Set default node environment to development
-process.env.NODE_ENV = 'production';
-//process.env.NODE_ENV || 'development';
+//process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var mongoose = require('mongoose');
