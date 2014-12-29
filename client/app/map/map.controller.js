1165709c72a9a84bcabd2fc191daa7b48c3b5fe1
Nathan Roberson <nathaniroberson@gmail.com>
2014-12-29 16:13:54 -0500

    Panning and zooming

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -3,30 +3,45 @@
 angular.module('whereisdrakeApp')
   .controller('MapCtrl', function ($scope, $http) {
 
-$http.get('/api/twitter').success(function(drake) {
-      $scope.tweets = drake;
-    });
-
     $scope.songkick = '';
     $http.get('/api/songkick')
        .success(function(data) {
         $scope.songkick = data;
+        update_center(data);
     }).error(function(error) {
      $scope.songkick = error;
     });
 
-   $scope.mapOptions = {
-          center: new google.maps.LatLng(36.109622,-115.1732124),
-          zoom: 17,
-          mapTypeId: google.maps.MapTypeId.SATELLITE,
-          scrollwheel: false,
-          scaleControl: false,
-          mapTypeControl: false,
-          draggable: false,
-          navigationControl: false,
-          streetViewControl: false,
-          zoomControl: false,
-          disableDefaultUI: true}
+ $scope.mapOptions = {
+              center: new google.maps.LatLng(0,0),
+              zoom: 2,
+              mapTypeId: google.maps.MapTypeId.SATELLITE,
+              scrollwheel: false,
+              scaleControl: false,
+              mapTypeControl: false,
+              draggable: false,
+              navigationControl: false,
+              streetViewControl: false,
+              zoomControl: false,
+              disableDefaultUI: true
+            };
+
+
+
+   function update_center(data){
+       var coordinates =  new google.maps.LatLng(data.venue.lat,data.venue.lng);
+       if (typeof $scope.myMap !== 'undefined'){
+          $scope.myMap.panTo(coordinates);
+          setTimeout(function() {$scope.myMap.setZoom(4)},1500);
+          setTimeout(function() {$scope.myMap.setZoom(6)},2000);
+          setTimeout(function() {$scope.myMap.setZoom(8)},2500);
+          setTimeout(function() {$scope.myMap.setZoom(10)},3000);
+          setTimeout(function() {$scope.myMap.setZoom(12)},3500);
+          setTimeout(function() {$scope.myMap.setZoom(15)},4000);
+          setTimeout(function() {$scope.myMap.setZoom(17)},4200);
+
+       }
+      };
 
   });
 
